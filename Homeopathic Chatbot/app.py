b97a6612a5af1782913f79fa0e6d6abd142b2647
Suchandra5 <suchandra0245@gmail.com>
2026-03-16 14:24:19 +0530

    Update app.py

--- Homeopathic Chatbot/app.py
+++ Homeopathic Chatbot/app.py
@@ -11,7 +11,7 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 st.title("🌿 Homeopathic Medicine Chatbot")
 
